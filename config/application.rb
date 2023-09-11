0f89107edab2c458839c898945cccb03a2f0d75f
gangelo <web.gma@gmail.com>
2023-09-11 18:32:06 -0400

    Set rubocop rules; fix rubocop violations

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ require 'action_cable/engine'
 Bundler.require(*Rails.groups)
 
 module Food
+  # This is the application class for this application.
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
