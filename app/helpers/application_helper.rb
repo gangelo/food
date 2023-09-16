1c0ddc2a571f0a6666b28c7d25afcea7141a6e22
gangelo <web.gma@gmail.com>
2023-09-16 12:54:39 -0400

    Enable/disable nav controls depending if we're on the current page or not

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@
 
 # This is the helper file for the application.
 module ApplicationHelper
+  # Returns the disabled css class to disable the control if the current page
+  # is equal to path.
+  #
+  # Usage:
+  #
+  # <%= link_to "Sign-up",
+  #     new_user_registration_path,
+  #     class: "btn btn-warning #{disabled_if_current_page(new_user_registration_path)}"
+  # %>
+  def disabled_if_current_page(path)
+    'disabled' if current_page?(path)
+  end
 end
